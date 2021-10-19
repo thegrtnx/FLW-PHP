16373141a93c49938031999750373438bed5164b
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-10-19 01:56:45 +0100

    new file

---
+++ list of nigerian banks.php
@@ -0,0 +1,250 @@
+<?php
+
+   $banks = array(
+    array('id' => '1','name' => 'Access Bank','code'=>'044'),
+    array('id' => '2','name' => 'Citibank','code'=>'023'),
+    array('id' => '3','name' => 'Diamond Bank','code'=>'063'),
+    array('id' => '4','name' => 'Dynamic Standard Bank','code'=>''),
+    array('id' => '5','name' => 'Ecobank Nigeria','code'=>'050'),
+    array('id' => '6','name' => 'Fidelity Bank Nigeria','code'=>'070'),
+    array('id' => '7','name' => 'First Bank of Nigeria','code'=>'011'),
+    array('id' => '8','name' => 'First City Monument Bank','code'=>'214'),
+    array('id' => '9','name' => 'Guaranty Trust Bank','code'=>'058'),
+    array('id' => '10','name' => 'Heritage Bank Plc','code'=>'030'),
+    array('id' => '11','name' => 'Jaiz Bank','code'=>'301'),
+    array('id' => '12','name' => 'Keystone Bank Limited','code'=>'082'),
+    array('id' => '13','name' => 'Providus Bank Plc','code'=>'101'),
+    array('id' => '14','name' => 'Polaris Bank','code'=>'076'),
+    array('id' => '15','name' => 'Stanbic IBTC Bank Nigeria Limited','code'=>'221'),
+    array('id' => '16','name' => 'Standard Chartered Bank','code'=>'068'),
+    array('id' => '17','name' => 'Sterling Bank','code'=>'232'),
+    array('id' => '18','name' => 'Suntrust Bank Nigeria Limited','code'=>'100'),
+    array('id' => '19','name' => 'Union Bank of Nigeria','code'=>'032'),
+    array('id' => '20','name' => 'United Bank for Africa','code'=>'033'),
+    array('id' => '21','name' => 'Unity Bank Plc','code'=>'215'),
+    array('id' => '22','name' => 'Wema Bank','code'=>'035'),
+    array('id' => '23','name' => 'Zenith Bank','code'=>'057'),
+    array('id' => '24','name' => 'HighStreet MFB bank','code'=>'090175'),
+    array('id' => '25','name' => 'TCF MFB','code' => '90115'),
+  array(
+      'id' => 132,
+      'code' => '560',
+      'name' => 'Page MFBank'
+  ),
+  array(
+      'id' => 133,
+      'code' => '304',
+      'name' => 'Stanbic Mobile Money'
+  ),
+  array(
+      'id' => 134,
+      'code' => '308',
+      'name' => 'FortisMobile'
+  ),
+  array(
+      'id' => 135,
+      'code' => '328',
+      'name' => 'TagPay'
+  ),
+  array(
+      'id' => 136,
+      'code' => '309',
+      'name' => 'FBNMobile'
+  ),
+  array(
+      'id' => 137,
+      'code' => '011',
+      'name' => 'First Bank of Nigeria'
+  ),
+  array(
+      'id' => 138,
+      'code' => '326',
+      'name' => 'Sterling Mobile'
+  ),
+  array(
+      'id' => 139,
+      'code' => '990',
+      'name' => 'Omoluabi Mortgage Bank'
+  ),
+  array(
+      'id' => 140,
+      'code' => '311',
+      'name' => 'ReadyCash (Parkway)'
+  ),
+  array(
+      'id' => 143,
+      'code' => '306',
+      'name' => 'eTranzact'
+  ),
+  array(
+      'id' => 145,
+      'code' => '023',
+      'name' => 'CitiBank'
+  ),
+  array(
+      'id' => 147,
+      'code' => '323',
+      'name' => 'Access Money'
+  ),
+  array(
+      'id' => 148,
+      'code' => '302',
+      'name' => 'Eartholeum'
+  ),
+  array(
+      'id' => 149,
+      'code' => '324',
+      'name' => 'Hedonmark'
+  ),
+  array(
+      'id' => 150,
+      'code' => '325',
+      'name' => 'MoneyBox'
+  ),
+  array(
+      'id' => 151,
+      'code' => '301',
+      'name' => 'JAIZ Bank'
+  ),
+    array(
+      'id' => 153,
+      'code' => '307',
+      'name' => 'EcoMobile'
+  ),
+  array(
+      'id' => 154,
+      'code' => '318',
+      'name' => 'Fidelity Mobile'
+  ),
+  array(
+      'id' => 155,
+      'code' => '319',
+      'name' => 'TeasyMobile'
+  ),
+  array(
+      'id' => 156,
+      'code' => '999',
+      'name' => 'NIP Virtual Bank'
+  ),
+  array(
+      'id' => 157,
+      'code' => '320',
+      'name' => 'VTNetworks'
+  ),
+    array(
+      'id' => 159,
+      'code' => '501',
+      'name' => 'Fortis Microfinance Bank'
+  ),
+  array(
+      'id' => 160,
+      'code' => '329',
+      'name' => 'PayAttitude Online'
+  ),
+  array(
+      'id' => 161,
+      'code' => '322',
+      'name' => 'ZenithMobile'
+  ),
+  array(
+      'id' => 162,
+      'code' => '303',
+      'name' => 'ChamsMobile'
+  ),
+  array(
+      'id' => 163,
+      'code' => '403',
+      'name' => 'SafeTrust Mortgage Bank'
+  ),
+  array(
+      'id' => 164,
+      'code' => '551',
+      'name' => 'Covenant Microfinance Bank'
+  ),
+  array(
+      'id' => 165,
+      'code' => '415',
+      'name' => 'Imperial Homes Mortgage Bank'
+  ),
+  array(
+      'id' => 166,
+      'code' => '552',
+      'name' => 'NPF MicroFinance Bank'
+  ),
+  array(
+      'id' => 167,
+      'code' => '526',
+      'name' => 'Parralex'
+  ),
+  array(
+      'id' => 169,
+      'code' => '084',
+      'name' => 'Enterprise Bank'
+  ),
+    array(
+      'id' => 187,
+      'code' => '314',
+      'name' => 'FET'
+  ),
+  array(
+      'id' => 188,
+      'code' => '523',
+      'name' => 'Trustbond'
+  ),
+  array(
+      'id' => 189,
+      'code' => '315',
+      'name' => 'GTMobile'
+  ),
+    array(
+      'id' => 182,
+      'code' => '327',
+      'name' => 'Pagatech'
+  ),
+  array(
+      'id' => 183,
+      'code' => '559',
+      'name' => 'Coronation Merchant Bank'
+  ),
+  array(
+      'id' => 184,
+      'code' => '601',
+      'name' => 'FSDH'
+  ),
+  array(
+      'id' => 185,
+      'code' => '313',
+      'name' => 'Mkudi'
+  ),
+   array(
+      'id' => 171,
+      'code' => '305',
+      'name' => 'Paycom'
+  ),
+  array(
+      'id' => 172,
+      'code' => '100',
+      'name' => 'SunTrust Bank'
+  ),
+  array(
+      'id' => 173,
+      'code' => '317',
+      'name' => 'Cellulant'
+  ),
+  array(
+      'id' => 174,
+      'code' => '401',
+      'name' => 'ASO Savings and & Loans'
+  ),
+  array(
+      'id' => 176,
+      'code' => '402',
+      'name' => 'Jubilee Life Mortgage Bank'
+  ),
+);
+for ($row = 0; $row < 68; $row++) {
+ 
+  
+    echo strtoupper($banks[$row]['name'])."<br/>";
+}
+  //echo $banks[0]['name'];
